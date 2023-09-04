044cf128259866946bf3867a54ca0a350814c1d5
Clifford Halasan <clifford.halasan@carsu.edu.ph>
2023-09-04 21:38:52 +0800

    using Axios

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,13 +1,11 @@
 const form = document.getElementById("form_sentence");
 
 if (form) {
-  form.onsubmit = function (e) {
+  form.onsubmit = async function (e) {
     e.preventDefault();
 
     const formData = new FormData(form);
 
-    for (const [key, value] of formData) {
-      console.log(`${key}: ${value}\n`);
-    }
+    console.log(await window.axios.openAI());
   };
 }
